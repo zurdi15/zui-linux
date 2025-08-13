09d897bdf854bc4c0674e830e0b3e972515eec4c
zurdi <contact@zurdi.dev>
2025-08-13 19:00:34 +0200

    style: update polybar configuration for improved layout and color scheme

--- themes/nord/polybar/launch.sh
+++ themes/nord/polybar/launch.sh
@@ -4,20 +4,21 @@
 killall -q polybar
 
 ## Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u "${UID}" -x polybar >/dev/null; do sleep 1; done
 
 TOP_BARS="${HOME}/.config/polybar/top_bars.ini"
 BOTTOM_BARS="${HOME}/.config/polybar/bottom_bars.ini"
 
 ## Top Right bars
-polybar cpu -c ${TOP_BARS} &
-polybar memory -c ${TOP_BARS} &
-polybar disk -c ${TOP_BARS} &
+polybar cpu -c "${TOP_BARS}" &
+polybar memory -c "${TOP_BARS}" &
+polybar disk -c "${TOP_BARS}" &
 
 ## Top Center bars
-polybar spotify -c ${TOP_BARS} &
+polybar date -c "${TOP_BARS}" &
 
 ## Bottom Left bars
+polybar spotify -c "${BOTTOM_BARS}" &
 
 ## Bottom Center bars
 
@@ -32,19 +33,18 @@ if [[ -z $(find /sys/class/backlight -type d -empty) ]]; then
 else
 	BACKLIGHT=''
 fi
-polybar notifications${BATTERY}${BACKLIGHT} -c ${BOTTOM_BARS} &
-polybar indicators${BATTERY}${BACKLIGHT} -c ${BOTTOM_BARS} &
-polybar network${BATTERY}${BACKLIGHT} -c ${BOTTOM_BARS} &
-polybar battery${BACKLIGHT} -c ${BOTTOM_BARS} &
-polybar audio${BACKLIGHT} -c ${BOTTOM_BARS} &
-polybar backlight -c ${BOTTOM_BARS} &
-polybar date -c ${BOTTOM_BARS} &
+polybar notifications"${BATTERY}""${BACKLIGHT}" -c "${BOTTOM_BARS}" &
+polybar indicators"${BATTERY}""${BACKLIGHT}" -c "${BOTTOM_BARS}" &
+polybar network"${BATTERY}""${BACKLIGHT}" -c "${BOTTOM_BARS}" &
+polybar battery"${BACKLIGHT}" -c "${BOTTOM_BARS}" &
+polybar audio"${BACKLIGHT}" -c "${BOTTOM_BARS}" &
+polybar backlight -c "${BOTTOM_BARS}" &
 
 ## Top Left bars
 if [[ -z ${SECONDARY_MONITOR} ]]; then
-	polybar workspaces-single-monitor -c ${TOP_BARS} &	
+	polybar workspaces-single-monitor -c "${TOP_BARS}" &
 else
-	polybar workspaces -c ${TOP_BARS} &
-	MAIN_MONITOR=${SECONDARY_MONITOR}
-	polybar workspaces -c ${TOP_BARS} &
+	polybar workspaces -c "${TOP_BARS}" &
+	export MAIN_MONITOR=${SECONDARY_MONITOR}
+	polybar workspaces -c "${TOP_BARS}" &
 fi
